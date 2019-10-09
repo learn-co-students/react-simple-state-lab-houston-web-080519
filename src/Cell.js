fd93fc2ed4eeb2b684826c57d1ff36c5bc4ee23d
Israel <Israel.pro1994@gmail.com>
2019-10-09 17:06:09 -0500

    Done.

---
+++ src/Cell.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+
+export default class Cell extends Component{
+    
+    constructor(props) {
+        super(props)
+        this.state = {
+          color: this.props.value
+        }
+      }
+      render(){
+        //   debugger
+          return (
+              <div className={"cell"} style={{backgroundColor: this.state.color}} onClick={()=> {this.setState({color:"#333"})}}></div>
+          )
+      }
+}
--- src/Matrix.js
+++ src/Matrix.js
@@ -1,21 +1,38 @@
 import React, { Component } from 'react';
+import Cell from "./Cell.js"
 
 export default class Matrix extends Component {
-  
-  genRow = (vals) => (
-    vals.map(val => <div className="cell"></div>) // replace me and render a cell component instead!
-  )
-  
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      stuff: props
+    }
+  }
+
+  // genRow = () => (
+    // this.props.values.map((val) => <Cell value={val}/>) // replace me and render a cell component instead!
+    genRow = vals => vals.map(val => <Cell value={val} />)
+  // )
+
   genMatrix = () => (
     this.props.values.map(rowVals => <div className="row">{this.genRow(rowVals)}</div>)
   )
-  
+
   render() {
+    // debugger
     return (
       <div id="matrix">
         {this.genMatrix()}
       </div>
     )
   }
-  
+
+}
+
+Matrix.defaultProps = {
+  values: (() => {
+    const defRow = ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'];
+    return new Array(10).fill(defRow)
+  })()
 }
